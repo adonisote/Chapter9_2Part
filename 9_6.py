0cb24d24f91a0e81501a75115b777ce4401875d3
adonisote <adonis.almagro@code.berlin>
2023-10-23 01:03:28 +0200

    method increment number

--- 9_6.py
+++ 9_6.py
@@ -11,7 +11,10 @@ class Restaurant:
 
     def set_number_served(self, new_value):
         self.number_served = new_value
-
+    def increment_number_served(self):
+        self.number_served += 20
+    def read_number_served(self):
+        print(f'The number of customer served is {self.number_served}')
 
 restaurant = Restaurant('Las delicias de alicia', 'latin')
 print(restaurant.number_served)
@@ -21,3 +24,9 @@ print(restaurant.number_served)
 
 restaurant.set_number_served(60)
 print(restaurant.number_served)
+
+restaurant.increment_number_served()
+restaurant.read_number_served()
+
+restaurant.increment_number_served()
+restaurant.read_number_served()
