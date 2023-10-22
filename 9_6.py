766c080e70506f71c3fad698d947e76a50085724
adonisote <adonis.almagro@code.berlin>
2023-10-23 00:59:24 +0200

    method for set number

--- 9_6.py
+++ 9_6.py
@@ -9,8 +9,15 @@ class Restaurant:
     def describe_restaurant(self):
         print(f'{self.name.title()} is open now!')
 
+    def set_number_served(self, new_value):
+        self.number_served = new_value
+
+
 restaurant = Restaurant('Las delicias de alicia', 'latin')
 print(restaurant.number_served)
 
 restaurant.number_served = 40
+print(restaurant.number_served)
+
+restaurant.set_number_served(60)
 print(restaurant.number_served)
